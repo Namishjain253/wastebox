f900d8732268d4d22364bb5cccda53c0d1d43dc0
Lalitha R <padmajothi020296@gmail.com>
2020-05-29 11:46:31 +0530

    bio waste services

--- skillassure-wastebox-businesslayer/src/main/java/com/skillassure/wastebox/businesslayer/biowaste/biodetails/BioDetailsService.java
+++ skillassure-wastebox-businesslayer/src/main/java/com/skillassure/wastebox/businesslayer/biowaste/biodetails/BioDetailsService.java
@@ -1,5 +1,29 @@
 package com.skillassure.wastebox.businesslayer.biowaste.biodetails;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillassure.wastebox.model.biowaste.biodetails.BioDetails;
+import com.skillassure.wastebox.repository.biowaste.biodetails.BioRepository;
+
+@Service
 public class BioDetailsService {
 
+	@Autowired
+	private BioRepository bioRepository;
+	
+	public BioDetails addbag(BioDetails bioDetails) {
+		return bioRepository.save(bioDetails);
+	}
+
+	public Optional<BioDetails> getBagById(long id) {
+		return bioRepository.findById(id);
+	}
+
+	public List<BioDetails> getAllBags() {
+		return bioRepository.findAll();
+	}
 }
---
+++ skillassure-wastebox-unittest/src/test/java/com/skillassure/wastebox/unittest/test.java
@@ -0,0 +1,35 @@
+package com.skillassure.wastebox.unittest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.skillassure.wastebox.businesslayer.biowaste.biodetails.BioDetailsService;
+import com.skillassure.wastebox.restapi.biowaste.biodeatils.BioDetailsController;
+
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
+//public class test {
+//	
+//	@Autowired
+//	public BioDetailsController bioDetailsController;
+//	
+//	@MockBean
+//	public BioDetailsService bioDetailService;
+//	
+//
+//	@Before
+//	public void setup() {
+//		
+//	}
+//	
+//	@Test
+//	public void check() {
+//		assertEquals(true, true);
+//	}
+//}
